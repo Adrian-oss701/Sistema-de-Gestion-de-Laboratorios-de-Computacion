d10b75c795731500fa7ce9f42a13bd0accc1fc9f
Adrian <adrianccl71@gmail.com>
2026-06-10 19:27:57 -0400

    avance

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"<h1>Funcionando con normalidad</h1>"
+    return render_template('index.html')
+
+@app.route('/laboratorios')
+def laboratorios():
+    return render_template('laboratorios.html')
+
+@app.route('/reservas')
+def reservas():
+    return render_template('reservas.html')
+
+@app.route('/incidencias')
+def incidencias():
+    return render_template('incidencias.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
